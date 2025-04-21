1a10feda1f829e1dfed2afb0e91ad2b6aa046905
NeodV <viter39@gmail.com>
2025-04-21 16:38:15 +0300

    2-я часть

--- Pelmeni/Pelmeni.cpp
+++ Pelmeni/Pelmeni.cpp
@@ -1,6 +1,14 @@
 ﻿#include <iostream>
+#include <locale.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "Rus");
+    Qf = af * Qp;
+    nf = Qf / qf;
+    Qt = (1 - af) * Qp;
+    nt = Qt / qt;
+    std::cout << "Количество пелменных автоматов: " << np << '\n';
+    std::cout << "Количество мясорубок: " << nf << '\n';
+    std::cout << "Количество тестомесителей: " << nt << '\n';
 }
